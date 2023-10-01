03d9696af9e3e0fc4ab7ad986713ee60ed0a3392
Nanichang Katzing <nanipaul68@gmail.com>
2023-10-01 16:59:47 +0100

    added migrations, models and seeders

---
+++ app/Models/Student.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Student extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'class_id',
+        'section_id',
+        'name',
+        'email'
+    ];
+}
